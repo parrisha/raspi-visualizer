c895888b6e657b48434cfa4d198464b88c0f2b37
parrisha <parrisha@vt.edu>
2017-04-29 18:38:46 -0400

    Bug fix - write all columns
    Iterate over all 16 columns

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from led import Matrix16x8
 #Will only be executed if this file is called directly from python
 if __name__ == '__main__':
    chunk = 4096
+   num_columns = 16
 
    #Setup for reading from a .wav file on disk
    wavfile = wave.open('/home/pi/ECE_612/samples/glass_animals.wav')
@@ -26,7 +27,7 @@ if __name__ == '__main__':
    display.set_brightness(1)
    display.write_display()
 
-   bin_mapping = spectrum.find_bin_mapping_np(16, chunk, sample_rate)
+   bin_mapping = spectrum.find_bin_mapping_np(num_columns, chunk, sample_rate)
    #Selected Bin Mapping:  [2, 3, 4, 7, 10, 16, 25, 38, 59, 90, 139, 215, 330, 509, 783, 1206, 1858]
    print("Selected Bin Mapping: ", bin_mapping)
 
@@ -41,9 +42,9 @@ if __name__ == '__main__':
 
       # Optional scale factor is applied to output of FFT
       #  4 is default for full scale 16-bit audio, increase if volume is really low
-      bin_powers = spectrum.get_spectrum(data, bin_mapping, chunk, scale=9)
+      bin_powers = spectrum.get_spectrum(data, bin_mapping, chunk, scale=10)
       print(bin_powers)
-      for col in range(0,8):
+      for col in range(0,num_columns):
          display.set_column(col, bin_powers[col])
       display.write_display()
       data = wavfile.readframes(chunk)
